9f5592871b6ae5180effbd18d327898c1daf7612
Noah Lidell <noahlidell@gmail.com>
2022-05-21 14:04:31 -0500

    nvimtree conf

--- init.lua
+++ init.lua
@@ -109,6 +109,8 @@ require('Comment').setup()
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.autowriteall = true
+vim.o.clipboard = "unnamedplus"
 
 --Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -342,8 +344,43 @@ local luasnip = require 'luasnip'
 require("bufferline").setup{
   options = { offsets = {{ filetype = "NvimTree", text = "", padding = 1 }}}
 }
-require("nvim-tree").setup{
+local nvimtreeopts = {
+   filters = {
+      dotfiles = false,
+      exclude = { "custom" },
+   },
+   disable_netrw = true,
+   hijack_netrw = true,
+   ignore_ft_on_setup = { "dashboard" },
+   open_on_tab = false,
+   hijack_cursor = false,
+   hijack_unnamed_buffer_when_opening = false,
+   update_cwd = true,
+   update_focused_file = {
+      enable = true,
+      update_cwd = false,
+   },
+   view = {
+      side = "left",
+      width = 25,
+      hide_root_folder = true,
+   },
+   git = {
+      enable = false,
+      ignore = true,
+   },
+   actions = {
+      open_file = {
+         resize_window = true,
+      },
+   },
+   renderer = {
+      indent_markers = {
+         enable = false,
+      },
+   },
 }
+require("nvim-tree").setup{nvimtreeopts}
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
