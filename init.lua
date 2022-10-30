204c359d0c4ed588a7b41bc55ac70f9155471a42
Noah Lidell <noahlidell@gmail.com>
2022-10-30 15:50:17 -0500

    haskell lang server

--- init.lua
+++ init.lua
@@ -487,13 +487,20 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
+-- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'hls' }
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
         capabilities = capabilities,
+        -- settings = {
+        --     languageServerHaskell = {
+        --         formattingProvider = "stylish-haskell";
+        --     };
+        -- };
     }
 end
+--
 require 'lspconfig'.hls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -503,7 +510,6 @@ require 'lspconfig'.hls.setup {
         };
     };
 }
-
 
 -- Example custom server
 -- Make runtime files discoverable to the server
